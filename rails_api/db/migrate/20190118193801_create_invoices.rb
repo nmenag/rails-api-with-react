ee0f070ae1cde13aeb2394db108317d0cf128fad
nmenag <nmena.garzon@gmail.com>
2019-01-18 20:47:42 +0100

    Add Invoice model

---
+++ rails_api/db/migrate/20190118193801_create_invoices.rb
@@ -0,0 +1,10 @@
+class CreateInvoices < ActiveRecord::Migration[5.2]
+  def change
+    create_table :invoices do |t|
+      t.integer :number, null: false
+      t.date :invoice_date, null: false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
