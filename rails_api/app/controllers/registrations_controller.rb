cb2fe889f689aac1f69f8837af24545de6d4480a
nmenag <nmena.garzon@gmail.com>
2019-01-18 23:11:41 +0100

    Add Registrations controller

---
+++ rails_api/app/controllers/registrations_controller.rb
@@ -0,0 +1,20 @@
+class RegistrationsController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
---
+++ rails_api/test/controllers/registrations_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class RegistrationsControllerTest < ActionDispatch::IntegrationTest
+  test 'POST#create: creates a new user' do
+    assert_difference 'User.count', 1, 'Expected to create a user' do
+      post registrations_url, xhr: true, as: :json,
+      params: { user: { email: 'new_user@test.com', password: '12345678' } }
+    end
+    user = User.find_by_email('new_user@test.com')
+
+    assert_response :success
+    assert_not_nil user
+    assert_not_nil user.auth_token
+  end
+end
