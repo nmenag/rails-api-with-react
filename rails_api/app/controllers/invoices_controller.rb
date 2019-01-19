443adb9b8430c3699fd6a6668475cc44e18555ef
nmenag <nmena.garzon@gmail.com>
2019-01-19 15:55:26 +0100

    Add tests for invoices controller

--- rails_api/app/controllers/invoices_controller.rb
+++ rails_api/app/controllers/invoices_controller.rb
@@ -1,7 +1,52 @@
 class InvoicesController < ApplicationController
   before_action :authenticate_user!
+  before_action :invoices_data, except: :create
 
   def index
-    render json: current_user.invoices.order_by_date
+    render json: @invoices.order_by_date
+  end
+
+  def show
+    invoice = @invoices.find_by(id: params[:id])
+    if invoice.present?
+      render json: invoice, status: :ok
+    else
+      render json: { errors: '404 not found'}, status: :not_found
+    end
+  end
+
+  def create
+    invoice = current_user.invoices.new(invoices_params)
+    if invoice.save
+      render json: invoice, status: :ok
+    else
+      render json: { errors: invoice.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    invoice = @invoices.find_by(id: params[:id])
+
+    if invoice.present? && invoice.update(invoices_params)
+      render json: invoice, status: :ok
+    else
+      render json: { errors: invoice.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    invoice = invoices_data.find(params[:id])
+    invoice.destroy
+    head :no_content
+  end
+
+  private
+
+  def invoices_params
+    params.require(:invoices).permit(:number, :invoice_date)
+  end
+
+  def invoices_data
+    @invoices = current_user.invoices
   end
 end
--- rails_api/test/controllers/invoices_controller_test.rb
+++ rails_api/test/controllers/invoices_controller_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class InvoicesControllerTest < ActionDispatch::IntegrationTest
-
   test 'GET#index return invoices of current user' do
     user = users(:user_1)
     get invoices_url, xhr: true, headers: { 'HTTP_AUTHORIZATION': user.auth_token }
@@ -9,5 +8,91 @@ class InvoicesControllerTest < ActionDispatch::IntegrationTest
     body = JSON.parse(response.body)
     assert_not_nil body
     assert_equal user.invoices.size, body.size
+  end
+
+  test 'GET#index without credentials return 401 Unauthorized' do
+    get invoices_url, xhr: true
+    assert_response :unauthorized
+  end
+
+  test 'GET#show return an invoice of current user' do
+    user = users(:user_1)
+    invoice = invoices(:invoice_1)
+    get invoice_url(invoice), xhr: true, headers: { 'HTTP_AUTHORIZATION': user.auth_token }
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_not_nil body
+    assert_equal invoice.id, body['id']
+  end
+
+  test 'GET#show without credentials return 401 Unauthorized' do
+    invoice = invoices(:invoice_1)
+    get invoice_url(invoice), xhr: true
+    assert_response :unauthorized
+  end
+
+  test 'GET#show without record exist return 404 not found' do
+    user = users(:user_1)
+    get invoice_url(23), xhr: true, headers: { 'HTTP_AUTHORIZATION': user.auth_token }
+    assert_response :not_found
+  end
+
+  test 'POST#create creates a new invoice' do
+    assert_difference 'Invoice.count', 1, 'Expected to create an invoice' do
+      post invoices_url, xhr: true, as: :json, params: {
+          invoices: {  number: 4, invoice_date: Date.today }
+        }, headers: { 'HTTP_AUTHORIZATION': users(:user_1).auth_token }
+    end
+
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal 4, body['number']
+    assert_equal Date.today.strftime, body['invoice_date']
+  end
+
+  test 'POST#create without credentials returns 401 Unauthorized' do
+    post invoices_url, xhr: true, as: :json, params: {
+      invoices: {  number: 4, invoice_date: Date.today }
+    }
+
+    assert_response :unauthorized
+  end
+
+  test 'PUT#update updates calories entry' do
+    invoice = invoices(:invoice_1)
+    user = invoice.user
+    put invoice_url(invoice), xhr: true, as: :json, params: {
+      invoices: {  number: 5, invoice_date: Date.today + 1.day }
+    }, headers: { 'HTTP_AUTHORIZATION': user.auth_token }
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal 5, body['number']
+    assert_equal (Date.today + 1.day).strftime, body['invoice_date']
+  end
+
+  test 'PUT#update without credentials returns 401 Unauthorized' do
+    invoice = invoices(:invoice_1)
+    put invoice_url(invoice), xhr: true, as: :json, params: {
+      invoices: {  number: 5, invoice_date: Date.today + 1.day }
+    }
+
+    assert_response :unauthorized
+  end
+
+  test 'DELETE#destroy destroy invoice' do
+    invoice = invoices(:invoice_1)
+    user = invoice.user
+    assert_difference 'Invoice.count', -1, 'Expected to destroy an invoice' do
+      delete invoice_url(invoice), xhr: true, headers: {
+        'HTTP_AUTHORIZATION': user.auth_token
+      }
+    end
+    assert_response :success
+  end
+
+  test 'Delete#destroy invoice without credential return 401 Unauthorized' do
+    invoice = invoices(:invoice_1)
+    delete invoice_url(invoice), xhr: true
+    assert_response :unauthorized
   end
 end
